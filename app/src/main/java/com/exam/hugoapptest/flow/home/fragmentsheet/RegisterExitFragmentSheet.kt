a58ca96a08d37fef8a1e41b96791ca82303bbca9
Francisco Hernandez <packoht@gmail.com>
2020-10-03 14:00:21 -0500

    feature | add liveData to validate data | Francisco Hernandez

--- app/src/main/java/com/exam/hugoapptest/flow/home/fragmentsheet/RegisterExitFragmentSheet.kt
+++
@@ -1,7 +0,0 @@
-package com.exam.hugoapptest.flow.home.fragmentsheet
-
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-
-class RegisterExitFragmentSheet : BottomSheetDialogFragment() {
-
-}
--- app/src/main/java/com/exam/hugoapptest/flow/home/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/exam/hugoapptest/flow/home/viewModel/RegisterViewModel.kt
@@ -1,10 +1,22 @@
 package com.exam.hugoapptest.flow.home.viewModel
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import com.exam.hugoapptest.base.BaseViewModel
+import com.exam.hugoapptest.extensions.isNotNullOrBlank
 import javax.inject.Inject
 
 class RegisterViewModel @Inject constructor(
 
 ) : BaseRegisterViewModel()  {
 
+    val carRegistrationNumber = MutableLiveData<String>()
+
+    var isButtonRegisterEnable = MediatorLiveData<Boolean>().apply {
+        addSource(carRegistrationNumber) { value = validateCompleteData() }
+    }
+
+    private fun validateCompleteData() : Boolean {
+        return carRegistrationNumber.value.isNotNullOrBlank()
+    }
 }
